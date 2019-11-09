8cfd18eeeb28960355b875fcc3e07d8eb896c595
LuibomyrKinash <55803759+LuibomyrKinash@users.noreply.github.com>
2019-11-09 23:29:36 +0200

    Delete script.js

--- ТASK_5/JS/script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('resize', function(){
-    if(this.innerHeight < 768){
-
-        document.getElementById("test").classList.add('text');
-     }else{
-         console.log("ALL GOOD");
-     }
- });
