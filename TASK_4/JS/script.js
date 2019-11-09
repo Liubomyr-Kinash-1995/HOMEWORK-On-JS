fbe3c884091b85929ae46a11398cc0e22a4b2bfd
LuibomyrKinash <55803759+LuibomyrKinash@users.noreply.github.com>
2019-11-09 23:29:17 +0200

    Delete script.js

--- TASK_4/JS/script.js
+++
@@ -1,9 +0,0 @@
-tog.addEventListener('scroll', function() {
-    if (this.scrollTop < 200) {
-        document.getElementById("text").classList.remove('text2');
-    } else if (this.scrollTop > 200) {
-        document.getElementById("text").classList.add("text2")
-        console.log("SCROLL");
-
-    }
-});
