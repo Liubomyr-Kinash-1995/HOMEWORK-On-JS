1b8af5c13edb72d695609ca8f657793a483097aa
LuibomyrKinash <55803759+LuibomyrKinash@users.noreply.github.com>
2019-11-09 23:26:08 +0200

    JS JQery

---
+++ Home_9/TASK_2/JS/script.js
@@ -0,0 +1,13 @@
+$("<p>5: One </p>").insertAfter("h2");
+$("<p>4: TWO</p>").insertAfter("h2");
+$("<p>3: Three</p>").insertAfter("h2");
+$("<p>2: FOur</p>").insertAfter("h2");
+$("<p>1: Five</p>").insertAfter("h2");
+
+
+
+$(".container").append('<p>1.Hi</p>');
+$(".container").append('<p>2.Hi</p>');
+$(".container").append('<p>3.Hi</p>');
+$(".container").append('<p>4.Hi</p>');
+$(".container").append('<p>5.Hi</p>');
